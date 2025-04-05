b32b36a00432f2c1e21f682eedaf15252c5891c2
Naman-Goel-iitm <22f2001226@ds.study.iitm.ac.in>
2025-04-06 00:31:04 +0530

    describe your changes here

--- src/App.tsx
+++ src/App.tsx
@@ -742,7 +742,7 @@ function App() {
             <div className="flex justify-between h-16">
               <div className="flex">
                 <div className="flex-shrink-0 flex items-center">
-                  <span className="text-xl font-bold text-google-blue">Google Arcade</span>
+                  <span className="text-xl font-bold text-google-blue">Being Notified</span>
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                   <Link
